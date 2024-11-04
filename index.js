836e36c764fd6513d7c8dc74271244345695c627
Matheus <matheusopi18@gmail.com>
2024-11-04 19:08:19 -0400

    caminho do index.html resolvido

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '.')));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/' , (req, res)=>{
-    res.sendFile(path.join(__dirname, '../index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Caminho atualizado
 });
 
 app.post('/solicitar', (req, res) => {
